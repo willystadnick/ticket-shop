2ee657d3aa939cfcda05d8fccf5612b84e6e889a
Willy Stadnick <willy.stadnick@gmail.com>
2016-05-03 20:52:06 -0300

    Remove form errors listing

--- resources/views/eventos/form.blade.php
+++ resources/views/eventos/form.blade.php
@@ -1,11 +1,3 @@
-@if ($errors->any())
-    <ul class="alert alert-danger">
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-@endif
-
 <div class="form-group {{ $errors->has('nome') ? 'has-error' : ''}}">
     {!! Form::label('nome', trans('eventos.nome'), ['class' => 'col-sm-3 control-label']) !!}
     <div class="col-sm-6">
